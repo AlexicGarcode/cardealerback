74918f9320ddfb91ea9c96dcc02782e9a8a278bd
AlexAutomation <80054118+AlexicGarcode@users.noreply.github.com>
2023-01-09 13:23:09 -0600

    se actualiza model.routes.js

--- routes/model.routes.js
+++ routes/model.routes.js
@@ -2,22 +2,18 @@ const { Router } = require('express');
 const { body, check } = require('express-validator');
 const bodyParser = require('body-parser');
 const { validateFields, validateJWT } = require('../middlewares');
-const { modelsPost, modelsPut, modelsPaginatePost} = require('../controllers/model.controllers');
+const { modelsGetById, modelsPost, modelsPut, modelsPaginatePost } = require('../controllers/model.controllers');
 const  jsonParser = bodyParser.json();
 
 const { modelExists, brandExists } = require('../helpers/validate-db');
 
 const router = Router(); 
 
-/* router.get(
-  "/:id",
-  [
-    check("id", "No Mongo id").isMongoId(),
-    check("id").custom(modelExists),
-    validateFields,
-  ],
-  modelsGetById
-); */ 
+router.get('/:id', [    
+    check('id','No Mongo id').isMongoId(), 
+    check('id').custom(modelExists),
+    validateFields
+], modelsGetById );
 
 router.post('/',[validateJWT,
     jsonParser,
